563734a634497b223c9a7eef6cc303d76348a429
Julia Marina Ferecean <juliaferecean@gmail.com>
2024-06-27 21:46:41 +0200

    Login Button für Admin

--- .idea/Typescript/login.js
+++ .idea/Typescript/login.js
@@ -1,6 +1,23 @@
 document.addEventListener('DOMContentLoaded', function () {
+    // Auswahl der Buttons und Eingabefelder
     var guestLoginButton = document.querySelector('.guest-login');
+    var loginButton = document.querySelector('.login');
+    var usernameInput = document.getElementById('username');
+    var passwordInput = document.getElementById('password');
+    // Event-Listener für den Gast-Login-Button
     guestLoginButton.addEventListener('click', function () {
-        window.location.href = 'home.html';
+        window.location.href = 'home.html'; // Weiterleitung zur Gast-Login-Seite
+    });
+    // Event-Listener für den Login-Button
+    loginButton.addEventListener('click', function (event) {
+        event.preventDefault(); // Verhindern des Standardverhaltens des Formular-Submit-Buttons
+        var username = usernameInput.value;
+        var password = passwordInput.value;
+        if (username === 'admin123' && password === 'passwort1') {
+            window.location.href = 'startseite.html'; // Weiterleitung zur Admin-Seite bei erfolgreicher Anmeldung
+        }
+        else {
+            alert('Invalid username or password'); // Fehlermeldung bei ungültigen Anmeldeinformationen
+        }
     });
 });
--- .idea/Typescript/login.ts
+++ .idea/Typescript/login.ts
@@ -1,7 +1,26 @@
 document.addEventListener('DOMContentLoaded', () => {
+    // Auswahl der Buttons und Eingabefelder
     const guestLoginButton = document.querySelector('.guest-login') as HTMLButtonElement;
+    const loginButton = document.querySelector('.login') as HTMLButtonElement;
+    const usernameInput = document.getElementById('username') as HTMLInputElement;
+    const passwordInput = document.getElementById('password') as HTMLInputElement;
 
+    // Event-Listener für den Gast-Login-Button
     guestLoginButton.addEventListener('click', () => {
-        window.location.href = 'home.html';
+        window.location.href = 'home.html'; // Weiterleitung zur Gast-Login-Seite
+    });
+
+    // Event-Listener für den Login-Button
+    loginButton.addEventListener('click', (event) => {
+        event.preventDefault(); // Verhindern des Standardverhaltens des Formular-Submit-Buttons
+
+        const username = usernameInput.value;
+        const password = passwordInput.value;
+
+        if (username === 'admin123' && password === 'passwort1') {
+            window.location.href = 'startseite.html'; // Weiterleitung zur Admin-Seite bei erfolgreicher Anmeldung
+        } else {
+            alert('Invalid username or password'); // Fehlermeldung bei ungültigen Anmeldeinformationen
+        }
     });
 });
